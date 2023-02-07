2f57a1071fa92519fa5b76f0a4f33b8e65915e2f
Brian <wamwangimathenge@gmail.com>
2023-02-07 18:36:37 +0300

    Looping with #times

--- run.rb
+++ run.rb
@@ -1,12 +1,19 @@
+# While construct
 i = 0
 while i < 5
     puts "Looping!"
     i += 1
 end
 
-
+# Until loop
 counter = 0
 until counter == 10
     puts "Counting!"
     counter += 1
+end
+
+# For Loop
+10.times do |i|
+    puts "Looping"
+    puts "i is: #{i}"
 end
